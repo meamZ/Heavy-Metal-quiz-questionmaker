8a724b8577813c6c84fb78b36180202618f52ea0
Michael Z <m.zinsmeister@live.de>
2017-08-08 22:44:51 +0200

    hacked it together

---
+++ app.js
@@ -0,0 +1,26 @@
+function handleInput() {
+  $('form').submit(function(event){
+    event.preventDefault();
+    var question = '\''+$(this).find('#questionin').val()+'\'';
+    var answers =  ['\''+$(this).find('#answer1').val()+'\'','\''+$(this).find('#answer2').val()+'\'',
+    '\''+$(this).find('#answer3').val()+'\'','\''+$(this).find('#answer4').val()+'\''];
+
+    renderOut(question, answers, $('code').text() === '');
+  });
+}
+
+function renderOut(question, answers,isfirst){
+  var first = '{question: ';
+  if(!isfirst){
+    first ='\n{question: ';
+  }
+  $('.output').append(first+question+',\n'+
+  'answers: ['+answers+'],\n'+
+  'correctanswer: '+answers[0]+',\n'+
+  'wasasked: false,},');
+  $('form input').val('');
+}
+
+$(function() {
+  handleInput();
+});
